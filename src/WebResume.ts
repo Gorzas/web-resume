3aa0af1dc935b52a3b1f9a1184acd68ddcb64fbf
JD Cano <joseda87@gmail.com>
2024-06-20 07:55:07 +0200

    chore: set two columns using flex

--- src/WebResume.ts
+++ src/WebResume.ts
@@ -1,27 +1,38 @@
 import { html, css, LitElement } from 'lit';
-import { property } from 'lit/decorators.js';
 
 export class WebResume extends LitElement {
   static styles = css`
     :host {
-      display: block;
-      padding: 25px;
       color: var(--web-resume-text-color, #000);
+      display: flex;
+      padding: 25px;
+      margin: 0 auto;
+      max-width:800px;
+    }
+
+    aside {
+      background: green;
+      flex: 2;
+    }
+
+    article {
+      flex: 4;
+      padding: 0 5rem;
+    }
+
+    header {
+      border-bottom: 1px solid #000;
     }
   `;
 
-  @property({ type: String }) header = 'Hey there';
-
-  @property({ type: Number }) counter = 5;
-
-  __increment() {
-    this.counter += 1;
-  }
-
   render() {
     return html`
-      <h2>${this.header} Nr. ${this.counter}!</h2>
-      <button @click=${this.__increment}>increment</button>
+      <aside></aside>
+      <article>
+        <header>
+          <h1>José David Cano Pérez</h1>
+        </header>
+      </article>
     `;
   }
 }
